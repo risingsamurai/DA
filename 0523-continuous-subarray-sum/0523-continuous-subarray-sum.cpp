82fae728ee69f1fdc1bc5fab7e4a2f0986fc98bc
Aditya kundap <adityaskundap@gmail.com>
2026-08-12 23:29:16 +0530

    Time: 152 ms (5.73%), Space: 148.1 MB (13.52%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        map<int,int>mp;
+        int sum=0;
+        int result=0;
+        int remainder=0;
+        mp[0]=-1;
+        for(int i=0;i<nums.size();i++){
+             sum+=nums[i]; 
+             remainder=sum%k;
+             if(mp.find(remainder)!=mp.end()){ // if remainder is seen 
+            if(i-mp[remainder]>=2) //the condition
+                return true;
+             }else{
+                    mp[remainder]=i;
+                }
+              }
+         return false;
+    }
+};
