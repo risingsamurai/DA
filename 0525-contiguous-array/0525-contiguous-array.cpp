e408cfeb8ec0cb1060c8ae1c9aaa56f3215e1cd6
Aditya kundap <adityaskundap@gmail.com>
2026-08-13 21:28:30 +0530

    Time: 52 ms (5.27%), Space: 88.6 MB (13.29%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int Maxresult=0;
+        int cumSum=0;
+        map<int,int>mp;
+        mp[0] = -1;
+        for(int i=0;i<nums.size();i++){
+
+            if(nums[i]==0)
+               cumSum+=-1;
+            else 
+               cumSum+=1;
+
+            if(mp.find(cumSum)==mp.end()){ //if not found insert this 
+                mp[cumSum]=i;
+            }   
+
+             Maxresult = max(Maxresult, i - mp[cumSum]);   
+        }
+        return Maxresult;
+    }
+};
+/*so idea is basic frm past sums we hve done 560 nd target sum it is the same pattern but here we consider 0 as -1 so tht (1,0,0,1) this can become (1,-1,-1,1) and we can sum it to 0 and identify equal number of 0s and 1s and yeah we use the similar pattern logic of putting valus in map and moving forward and we we get the same value somewhere we calculate the distance frm tht to current as i-mp[cumSum] which gives us the longest array with max */
