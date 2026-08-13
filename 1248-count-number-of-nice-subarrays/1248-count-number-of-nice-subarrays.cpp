0e78289fc7c2f3f0bf5991e67776e25a65ea3a4b
Aditya kundap <adityaskundap@gmail.com>
2026-08-14 02:45:45 +0530

    Time: 117 ms (5.04%), Space: 106.4 MB (5.74%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -5,7 +5,7 @@ public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int result=0;
         int sum=0;
-        map<int,int>mp;
+       unordered_map<int,int>mp;
         mp[0]=1;
         for(int i=0 ; i<nums.size();i++){
             
