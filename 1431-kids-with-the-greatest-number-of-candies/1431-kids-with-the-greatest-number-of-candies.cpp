56800577d9a89dc2dbd1b5cbb324e36d9cbaec06
Aditya kundap <adityaskundap@gmail.com>
2026-08-19 17:56:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (60.55%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,3 +1,5 @@
+//Can this kid have at least as many candies as the current greatest kid or current max if yes then return true 
+
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
