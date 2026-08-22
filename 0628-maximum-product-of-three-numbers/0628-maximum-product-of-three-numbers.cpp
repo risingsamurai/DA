a31553e09f2dea2620ec18ec18fc614a564c0f22
Aditya kundap <adityaskundap@gmail.com>
2026-08-22 22:48:35 +0530

    Time: 14 ms (23.78%), Space: 31.5 MB (49.03%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -8,3 +8,7 @@ public:
                    nums[n-1] * nums[n-2] * nums[n-3]);
     }
 };
+
+/*eg:[-10, -9, 2, 3, 5]
+ smallest × second smallest × largest in max logic nums[0] * nums[1] * nums[n-1], 
+ this is the case if negative nd postitive also to counter tht and for normal we use   nums[n-1] * nums[n-2] * nums[n-3]) which mean largest , second largest andd third largest after sorting */
