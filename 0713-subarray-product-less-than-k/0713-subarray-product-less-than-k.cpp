9e9eea481a52b0b8e76fb28b261ccb222a09105d
Aditya kundap <adityaskundap@gmail.com>
2026-08-26 17:57:40 +0530

    Time: 4 ms (23.26%), Space: 44.9 MB (13.77%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,int>mp;
         int count=0;
         int prod=1;
         int left=0;
+ /* using basic sliding window concept till the valid condition */       
 
         for(int i=0;i<n;i++){
             prod*=nums[i];
-            while(left<=i && prod>=k){
+            while(left<=i && prod>=k){//left<=i is to check single ele if > than k
             prod = prod / nums[left]; //removes left most element 
              left++;
            }
