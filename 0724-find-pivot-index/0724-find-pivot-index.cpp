db3c636c6495ca797aeb20c552112b178f061223
Aditya kundap <adityaskundap@gmail.com>
2026-09-06 22:39:23 +0530

    Time: 0 ms (100%), Space: 35.6 MB (96.68%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int leftsum=0;
+        int rightsum=0;
+        int totalsum=0;
+        int pivot=0;
+        for(int i=0;i<n;i++){
+            totalsum+=nums[i];
+         }
+        for(int i=0;i<n;i++){
+
+            rightsum=totalsum-nums[i]-leftsum;
+
+            if(rightsum==leftsum){
+                return i;
+            }
+
+            leftsum+=nums[i];     /*left after everything bcuz we shouldnt calculate the current index */
+        }
+        return -1;
+    }
+};
